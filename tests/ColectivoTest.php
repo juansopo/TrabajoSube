6c84b05577202e5c1873aac82d81fdc2c63d6164
Franco Franzini <francofranzini12@gmail.com>
2023-08-31 18:54:36 -0300

    Primer prueba pasada

--- tests/ColectivoTest.php
+++ tests/ColectivoTest.php
@@ -1,9 +1,23 @@
 <?php 
 
 namespace TrabajoSube;
-
+require_once 'src/Colectivo.php';
+require_once 'src/Tarjeta.php';
+require_once 'src/Boleto.php';
 use PHPUnit\Framework\TestCase;
 
+
 class ColectivoTest extends TestCase{
+    public function testPagarCon(){
+        $colectivoTest = new Colectivo;
+        $tarjetaTest = new Tarjeta;
+
+        $boleto = $colectivoTest->tarifa;
+
+        //verificar que se este cargando correctamente la tarifa
+        $this->assertEquals($boleto, 120);
+
+        //
+    }
    
 }
